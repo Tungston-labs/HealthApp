85e6ae9869f18e886503d3ab309bb32ff0482422
Tungston-labs <support@tungstonlabs.com>
2025-12-22 13:05:57 +0530

    changes made in the delete view

--- plan/views.py
+++ plan/views.py
@@ -1,11 +1,17 @@
 # plans/views.py
-from rest_framework import generics
 from .models import Plan
 from .serializers import PlanSerializer,PlanMiniSerializer
 from accounts.permissions import IsAdmin,IsUser
 from django.shortcuts import get_object_or_404
 from accounts.paginations import CustomPagination
 from django.db.models import Count, Q
+from rest_framework import generics, status
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from .models import Plan
+from trainer.models import Trainer
+from client.models import Client
+from .serializers import PlanSerializer
 
 class PlanListCreateView(generics.ListCreateAPIView):
     queryset = Plan.objects.annotate(
@@ -20,12 +26,43 @@ class PlanListCreateView(generics.ListCreateAPIView):
     pagination_class = CustomPagination
 
     
+
+
+
 class PlanRetrieveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = PlanSerializer
     permission_classes = [IsAdmin]
 
     def get_object(self):
         return get_object_or_404(Plan, id=self.kwargs["pk"])
+
+    def destroy(self, request, *args, **kwargs):
+        plan = self.get_object()
+
+        # ✅ Check trainers linked to this plan
+        trainer_exists = Trainer.objects.filter(training_field=plan).exists()
+
+        # ✅ Check users/clients linked to this plan
+        user_exists = Client.objects.filter(plan=plan).exists()
+
+        if trainer_exists or user_exists:
+            return Response(
+                {
+                    "success": False,
+                    "message": "This plan cannot be deleted because trainers or users are assigned to it."
+                },
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        plan.delete()
+        return Response(
+            {
+                "success": True,
+                "message": "Plan deleted successfully."
+            },
+            status=status.HTTP_200_OK
+        )
+
     
 class PlanMiniListView(generics.ListAPIView):
     queryset = Plan.objects.all()
