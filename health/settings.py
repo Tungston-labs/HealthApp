29096cc8f74a41261fd73d1c77d4942cefffe398
Tungston-labs <support@tungstonlabs.com>
2026-01-03 12:16:39 +0530

    rechanged views

--- health/settings.py
+++ health/settings.py
@@ -34,9 +34,6 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='', cast=Csv())
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-RAZORPAY_KEY_ID = config('RAZORPAY_KEY_ID')
-RAZORPAY_KEY_SECRET = config('RAZORPAY_KEY_SECRET')
-
 
 
 # Application definition
