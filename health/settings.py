fd829a2e38d53405d727e8ff7b401e4580542acb
Tungston-labs <support@tungstonlabs.com>
2026-01-13 12:57:53 +0530

    changes in employee model

--- health/settings.py
+++ health/settings.py
@@ -125,8 +125,8 @@ REST_FRAMEWORK = {
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=30),   # or minutes=30, hours=1, days=1
-    'REFRESH_TOKEN_LIFETIME': timedelta(seconds=30),    # or days=30
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=12),   # or minutes=30, hours=1, days=1
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),    # or days=30
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
     'ALGORITHM': 'HS256',
