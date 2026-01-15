1851b2f5435b0c9b63f2e3b24a2096ee391db17b
Tungston-labs <support@tungstonlabs.com>
2026-01-15 16:06:51 +0530

    changed client serializer to add lat and long

--- trainer/serializers.py
+++ trainer/serializers.py
@@ -28,6 +28,18 @@ class TrainerSerializer(serializers.ModelSerializer):
         required=False
     )
     certificates_read = serializers.SerializerMethodField()
+    latitude = serializers.DecimalField(
+        max_digits=9,
+        decimal_places=6,
+        required=False,
+        allow_null=True
+    )
+    longitude = serializers.DecimalField(
+        max_digits=9,
+        decimal_places=6,
+        required=False,
+        allow_null=True
+    )
 
     password = serializers.CharField(write_only=True, required=False)
 
